799632eb544c34a779b221cb04e3ad3556f82b9b
陆离 <sirzenith@163.com>
2024-01-14 08:27:29 +0800

    bug fix(init): fix user config does not get merged into default config

--- lua/mongosh-nvim/init.lua
+++ lua/mongosh-nvim/init.lua
@@ -4,6 +4,25 @@ local log = require "mongosh-nvim.log"
 
 local M = {}
 
+---@param dst table
+---@param src table
+local function merge_tbl(dst, src)
+    for k, v in pairs(src) do
+        local old_v = dst[k]
+        local old_v_type = type(old_v)
+
+        if type(v) == "table" then
+            if old_v_type == "table" then
+                merge_tbl(old_v, v)
+            else
+                dst[k] = vim.deepcopy(v)
+            end
+        else
+            dst[k] = v
+        end
+    end
+end
+
 function M.register_cmd()
     require "mongosh-nvim.command"
 end
@@ -11,7 +30,7 @@ end
 ---@param options? table
 function M.setup(options)
     if options then
-        vim.tbl_extend("force", config, options)
+        merge_tbl(config, options)
     end
 
     if vim.fn.executable(config.executable) == 0 then
