0e434f7450410a755536e7ae27748067d97123aa
陆离 <sirzenith@163.com>
2024-09-12 20:12:43 +0800

    fix(treesitter): fix query mathes iteration

--- lua/mongosh-nvim/util/tree_sitter/base.lua
+++ lua/mongosh-nvim/util/tree_sitter/base.lua
@@ -9,7 +9,9 @@ function M.caps_to_table(query, captures)
 
     for index, node in ipairs(captures) do
         local name = query.captures[index]
-        tbl[name] = node
+        -- QUESTION: assigning simply `node` was always fine until neovim 0.10.1
+        -- is there any breaking change made to treesitter API?
+        tbl[name] = type(node) == "table" and node[1] or node
     end
 
     return tbl
@@ -24,6 +26,7 @@ end
 ---@field visited? table<string, boolean>
 
 ---@param contex mongo.treesitter.WalkThroughContext
+---@param node TSNode
 ---@return string?
 function M.walk_through_node(contex, node)
     local type = node:type()
